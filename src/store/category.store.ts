3564695cbf955ef14989994a01708c54093b323d
bulian1311 <bulian1311@gmail.com>
2022-04-11 15:07:13 +0300

    Types.

--- src/store/category.store.ts
+++ src/store/category.store.ts
@@ -10,7 +10,53 @@ export class CategoryStore {
   big = {
     id: "big-krvpdh",
     name: "Большие размеры",
-    categories: [],
+    categories: [
+      {
+        name: "Брюки",
+        id: "hdy76cct5e20",
+        icon: "pants",
+      },
+      {
+        name: "Пижамы",
+        id: "g853nd0f51dd",
+        icon: "pijamas",
+      },
+      {
+        name: "Туники",
+        id: "ggjf97g5e3mb",
+        icon: "blouse-2",
+      },
+      {
+        name: "Костюмы",
+        id: "g9mmas4413bd",
+        icon: "women-suit",
+      },
+      {
+        name: "Платья",
+        id: "v03mqiyz5x41",
+        icon: "cute-dress",
+      },
+      {
+        name: "Футболки",
+        id: "d00f73n4g1xa",
+        icon: "casual-t-shirt",
+      },
+      {
+        name: "Ночные сорочки",
+        id: "gown48d7vz",
+        icon: "dress-with-pockets",
+      },
+      {
+        name: "Сарафаны",
+        id: "d94n22g29raq",
+        icon: "sleeveless-drees",
+      },
+      {
+        name: "Халаты",
+        id: "4m98f4z1d0g2",
+        icon: "women-robe",
+      },
+    ],
   };
 
   fmale = {
---
+++ src/types/product.type.ts
@@ -0,0 +1,12 @@
+export type TProduct = {
+  price: string;
+  currency: string;
+  name: string;
+  material: string;
+  compoud: string;
+  color: string;
+  producer: string;
+  height: string;
+  size: string;
+  img: string;
+};
