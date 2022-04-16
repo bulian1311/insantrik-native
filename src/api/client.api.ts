b9c7a8b270f286d03b2c9ca3de788a9c25349582
bulian1311 <bulian1311@gmail.com>
2022-04-16 11:49:15 +0300

    Http client.

---
+++ src/api/client.api.ts
@@ -0,0 +1,25 @@
+import axios, { AxiosResponse, Method } from "axios";
+
+axios.interceptors.response.use(undefined, (error) => {
+  const { status } = error.response;
+  if (status === 404) {
+    console.error("Ошибка 404.");
+  }
+
+  if (status === 500) {
+    console.error("Ошибка 500.");
+  }
+
+  if (error.message === "Network Error") {
+    console.error("Ошибка сети.");
+  }
+
+  throw error.response;
+});
+
+export const api = axios.create({
+  baseURL:
+    "https://insantrik-shop-default-rtdb.europe-west1.firebasedatabase.app/",
+});
+
+export const fetchProducts = async () => {};
